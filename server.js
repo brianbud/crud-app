90f08696e306678620566c9cfc535a391f0a894f
Brian Bud <brian@brianbud.com>
2022-08-25 21:43:35 -0400

    testing server working

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('HELLO WORLD TEST');
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`The server is now running on port ${PORT}`);
+});
+
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
