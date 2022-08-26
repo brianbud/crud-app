6df4489baef4723d24e7b273c71927f48d5a7be1
Brian Bud <brian@brianbud.com>
2022-08-26 19:21:03 -0400

    use bodyParser to get input data in form

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`The server is now running on port ${PORT}`);
 });
+// be sure to place body-parser BEFORE your CRUD handlers
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//All handlers here
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html');
 });
 
 app.post('/quotes', (request, response) => {
-  console.log('HELLO WORLD TEST FROM CONSOLE!');
+  console.log(request.body);
 });
