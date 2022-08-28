fa0811acea22dbfd29d6d864732a5d0a597738c6
Brian Bud <brian@brianbud.com>
2022-08-27 20:35:01 -0400

    connect to mongodb database

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 8000;
+const MongoClient = require('mongodb').MongoClient;
+const connectionString =
+  'mongodb+srv://brian:<password>@cluster0.uhkkjle.mongodb.net/?retryWrites=true&w=majority'; //remember to add this in .env and gitignore
 
 app.listen(PORT, () => {
   console.log(`The server is now running on port ${PORT}`);
@@ -17,3 +20,9 @@ app.get('/', (request, response) => {
 app.post('/quotes', (request, response) => {
   console.log(request.body);
 });
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log('Connected to Database');
+  })
+  .catch((error) => console.error(error));
