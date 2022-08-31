21daa2163720c988559e32e5e3c329d7a0911255
Brian Bud <brian@brianbud.com>
2022-08-30 20:56:06 -0400

    put express request handlers into MongoClient call

--- server.js
+++ server.js
@@ -7,29 +7,28 @@ dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
 const connectionString = process.env.MONGODB_URI;
 
-app.listen(PORT, () => {
-  console.log(`The server is now running on port ${PORT}`);
-});
-// be sure to place body-parser BEFORE your CRUD handlers
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//All handlers here
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html');
-});
-
-app.post('/quotes', (request, response) => {
-  quotesCollection
-    .insertOne(request.body)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => console.error(error));
-});
-
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then((client) => {
     console.log('Connected to Database');
     const db = client.db('star-wars-quotes');
+    app.listen(PORT, () => {
+      console.log(`The server is now running on port ${PORT}`);
+    });
+    // be sure to place body-parser BEFORE your CRUD handlers
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    //All handlers here
+    app.get('/', (request, response) => {
+      response.sendFile(__dirname + '/index.html');
+    });
+
+    app.post('/quotes', (request, response) => {
+      quotesCollection
+        .insertOne(request.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+    });
   })
   .catch((error) => console.error(error));
