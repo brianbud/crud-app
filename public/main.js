488a7b55bbfd68c3b3f4b95cb492f0d04c531c2e
Brian Bud <brian@brianbud.com>
2022-09-06 21:32:56 -0400

    setting content type header for JSON

--- public/main.js
+++ public/main.js
@@ -3,5 +3,6 @@ const update = document.querySelector('#update-button');
 update.addEventListener('click', (_) => {
   fetch('/quotes', {
     method: 'put',
+    headers: { 'Content-Type': 'application/json' },
   });
 });
